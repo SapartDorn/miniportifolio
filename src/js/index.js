b5e92fcb099bce06ca1181a2b4aeb821b6d6de29
SapartDorn <leonardohlss14@gmai.com>
2025-04-28 19:52:55 -0300

    primeiro commit

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+// passo 1 - dar um jeito de pegar os elementos que representam as abas no HTML
+
+const abas = document.querySelectorAll(".aba");
+
+abas.forEach(aba) =
+
+
+// passo 2 - dar um jeito de identificar o clique no elemento da aba
